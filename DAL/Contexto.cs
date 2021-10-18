e850b3262f7ea1b8bb0f67010db3e3056c1160c8
Felix <felix_reynoso1@ucne.edu.do>
2021-10-18 02:21:11 -0400

    Agregando el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RolesConPermisos2.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RolesConPermisos2.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Permisos> Permisos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\BDRoles.db");
+        }
+    }
+}
